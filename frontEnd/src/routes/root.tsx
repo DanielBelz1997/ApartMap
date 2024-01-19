99d41c4de58c0a004b1b8f03a46be135c1e52477
BelzDaniel1997 <BelzDaniel1997@gmail.com>
2024-01-19 15:03:10 +0200

    changes

--- frontEnd/src/routes/root.tsx
+++ frontEnd/src/routes/root.tsx
@@ -10,7 +10,7 @@ export function Root() {
     if (mapDiv.current) {
       const webmap = new WebMap({
         portalItem: {
-          id: "aa1d3f80270146208328cf66d022e09c",
+          id: "e691172598f04ea8881cd2a4adaa45ba",
         },
       });
 
