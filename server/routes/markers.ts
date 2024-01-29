c63aed705c7a72e26067d6d5c797cb2cf86783cc
BelzDaniel1997 <BelzDaniel1997@gmail.com>
2024-01-30 00:42:29 +0200

    next step: react-query to frontend!

--- server/routes/markers.ts
+++ server/routes/markers.ts
@@ -1,21 +1,29 @@
-export async function markersRoutes(
-  fastify: {
-    [x: string]: any;
-    get: (
-      arg0: string,
-      arg1: (req: any, rep: any) => Promise<{ hello: string }>
-    ) => void;
-  },
-  options: any
-) {
-  fastify.get("/api/markers", async (req: any, rep: any) => {
-    const markers = fastify.mongo.client.db("VolnMap").collection("volnmap");
-    try {
-      const allMarkers = await markers.find({});
-      console.log(allMarkers);
-      return allMarkers;
-    } catch (err) {
-      return err;
+import {
+  FastifyInstance,
+  FastifyRequest,
+  FastifyReply,
+  FastifyError,
+} from "fastify";
+
+export async function markersRoutes(fastify: FastifyInstance, options: any) {
+  fastify.get(
+    "/api/markers",
+    async (req: FastifyRequest, rep: FastifyReply) => {
+      try {
+        const markers = await fastify?.mongo?.db
+          ?.collection("volnmap")
+          .find()
+          .toArray();
+
+        console.log(markers);
+
+        rep.send({ markers: markers }); // Returning markers array
+      } catch (err: any) {
+        console.error("Error retrieving markers:", err);
+        rep
+          .status(500)
+          .send({ error: `Internal Server Error: ${err.message}` });
+      }
     }
-  });
+  );
 }
